dfe5c7b95dc607a23a5f640dc1133ebfd4e5db1c
Pale91 <palmerale91@gmail.com>
2024-02-04 12:45:59 +0000

    Remove log

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,6 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-console.table(process.env);
-
 const handler = NextAuth({
   providers: [
     GoogleProvider({
