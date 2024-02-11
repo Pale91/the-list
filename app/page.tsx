7c73eeb72130f6770c192113fc556bd0105689c7
Pale91 <palmerale91@gmail.com>
2024-02-11 15:02:16 +0000

    Add daisy ui and navbar

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Image from 'next/image';
+import { MainContent } from './main-content';
+
+export default function Home() {
+  return (
+    <div>Content</div>
+  );
+}
