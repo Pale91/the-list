f0a384bf3c4fa6c089cc1e9ebf52213ab03e5fce
Pale91 <palmerale91@gmail.com>
2024-02-11 15:36:53 +0000

    Add navbar layout

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,62 +1,65 @@
-import React from "react";
+import React from 'react';
+import { Bars3Icon, UserCircleIcon } from '@heroicons/react/24/outline';
+
+const ListItems = () => (
+  <>
+    <li>
+      <a>The Dream</a>
+    </li>
+    <li>
+      <a>Fulfillment</a>
+    </li>
+    <li>
+      <a>My Gear</a>
+    </li>
+    <li>
+      <a>Store</a>
+    </li>
+    <li>
+      <a>About</a>
+    </li>
+    <li>
+      <a>Contacts</a>
+    </li>
+  </>
+);
 
 export const NavBar = ({ children }: React.PropsWithChildren) => {
   return (
     <div className="drawer">
-      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+      <input id="nav-bar-drawer" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col">
         {/* Navbar */}
         <div className="w-full navbar bg-base-300">
           <div className="flex-none lg:hidden">
             <label
-              htmlFor="my-drawer-3"
+              htmlFor="nav-bar-drawer"
               aria-label="open sidebar"
               className="btn btn-square btn-ghost"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block w-6 h-6 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-              </svg>
+              <Bars3Icon className="w-6 h-6" />
             </label>
           </div>
-          <div className="flex-1 px-2 mx-2">Navbar Title</div>
-          <div className="flex-none hidden lg:block">
+          <div className="navbar-start mx-2">The List</div>
+          <div className="navbar-center flex-none hidden lg:block">
             <ul className="menu menu-horizontal">
-              {/* Navbar menu content here */}
-              <li>
-                <a>Navbar Item 1</a>
-              </li>
-              <li>
-                <a>Navbar Item 2</a>
-              </li>
+            <ListItems />
             </ul>
+          </div>
+          <div className="navbar-end">
+            <UserCircleIcon className="w-6 h-6 mx-2" />
           </div>
         </div>
         {children}
       </div>
       <div className="drawer-side">
         <label
-          htmlFor="my-drawer-3"
+          htmlFor="nav-bar-drawer"
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
         <ul className="menu p-4 w-80 min-h-full bg-base-200">
-          {/* Sidebar content here */}
-          <li>
-            <a>Sidebar Item 1</a>
-          </li>
-          <li>
-            <a>Sidebar Item 2</a>
-          </li>
+          <ListItems />
         </ul>
       </div>
     </div>
