4857049bb7645ae468b025740a140c1e6f2ed8fc
Pale91 <palmerale91@gmail.com>
2024-03-09 13:15:19 +0000

    Fix build

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,25 @@ import { storage } from '../infrastructure/firebase';
 import { ref, getDownloadURL } from 'firebase/storage';
 
 export default async function Home() {
-  const imgUrlDesktop = await getDownloadURL(ref(storage, 'landing-dunas-side.jpg'));
-  const imgUrlMobile = await getDownloadURL(ref(storage, 'landing-dunas-back.jpg'));
+  const imgUrlDesktop = await getDownloadURL(
+    ref(storage, 'landing-dunas-side.jpg')
+  );
+  const imgUrlMobile = await getDownloadURL(
+    ref(storage, 'landing-dunas-back.jpg')
+  );
 
   return (
-    <div className='w-full'>
-      <img alt={'landing-dunas-back'} className='hidden md:block object-fill' src={imgUrlDesktop} />
-      <img alt={'landing-dunas-back'} className='block md:hidden object-fill' src={imgUrlMobile}/>
+    <div className="w-full">
+      <img
+        alt={'landing-dunas-back'}
+        className="hidden md:block object-fill"
+        src={imgUrlDesktop}
+      />
+      <img
+        alt={'landing-dunas-back'}
+        className="block md:hidden object-fill"
+        src={imgUrlMobile}
+      />
     </div>
   );
 }
