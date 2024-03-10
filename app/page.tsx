a9028834d29d6bd7c3720d65d901a42f344df4f5
Pale91 <palmerale91@gmail.com>
2024-03-10 17:03:58 +0000

    Refactor Banner component

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,8 @@
 import React from 'react';
-import { storage } from '../infrastructure/firebase';
-import { ref, getDownloadURL } from 'firebase/storage';
-import Image from 'next/image';
+import Banner from '@/components/landing/banner';
 
 export default async function Home() {
-  const imgUrlDesktop = await getDownloadURL(
-    ref(storage, 'landing-dunas-side.jpg')
-  );
-  const imgUrlMobile = await getDownloadURL(
-    ref(storage, 'landing-dunas-back.jpg')
-  );
-
   return (
-    <div className="w-full relative h-96">
-      <Image
-        alt={'landing-dunas-back'}
-        className="object-cover"
-        src={imgUrlDesktop}
-        fill
-        sizes="100%"
-        priority
-      />
-    </div>
+    <Banner />
   );
 }
