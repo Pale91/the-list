34e17c99c024cc4699e2d9834457cf7a23a6ccdf
Pale91 <palmerale91@gmail.com>
2024-03-10 18:14:58 +0000

    Fix build

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,5 @@ import React from 'react';
 import Banner from '@/components/landing/banner';
 
 export default async function Home() {
-  return (
-    <Banner />
-  );
+  return <Banner />;
 }
--- components/nav-bar/user-menu.tsx
+++ components/nav-bar/user-menu.tsx
@@ -3,6 +3,7 @@ import { UserCircleIcon } from '@heroicons/react/24/outline';
 import { getServerSession } from 'next-auth';
 import Link from 'next/link';
 import { SignOutButton } from './signout-button';
+import Image from 'next/image';
 
 export async function UserMenu() {
   const userSession = await getServerSession(authOptions);
@@ -23,7 +24,7 @@ export async function UserMenu() {
         className="btn btn-ghost btn-circle avatar"
       >
         <div className="w-10 rounded-full">
-          <img src={userSession.user.image} />
+          <Image alt="user-avatar" src={userSession.user.image} fill />
         </div>
       </div>
       <ul
