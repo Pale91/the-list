3978d9c52d42c4e016c7935b9b5c594e337a2d0b
Pale91 <palmerale91@gmail.com>
2024-03-10 18:04:03 +0000

    Add user menu

---
+++ components/nav-bar/user-menu.tsx
@@ -0,0 +1,39 @@
+import { authOptions } from '@/app/api/auth/[...nextauth]/auth-options';
+import { UserCircleIcon } from '@heroicons/react/24/outline';
+import { getServerSession } from 'next-auth';
+import Link from 'next/link';
+import { SignOutButton } from './signout-button';
+
+export async function UserMenu() {
+  const userSession = await getServerSession(authOptions);
+
+  if (!userSession) {
+    return (
+      <Link href={'/api/auth/signin'}>
+        <UserCircleIcon className="w-6 h-6 mx-2" />
+      </Link>
+    );
+  }
+
+  return (
+    <div className="dropdown dropdown-end">
+      <div
+        tabIndex={0}
+        role="button"
+        className="btn btn-ghost btn-circle avatar"
+      >
+        <div className="w-10 rounded-full">
+          <img src={userSession.user.image} />
+        </div>
+      </div>
+      <ul
+        tabIndex={0}
+        className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+      >
+        <li>
+          <SignOutButton />
+        </li>
+      </ul>
+    </div>
+  );
+}
