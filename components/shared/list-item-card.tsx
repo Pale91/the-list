83fb3698ac6ccf693312aa6d5678d10d8a2e9c68
Pale91 <palmerale91@gmail.com>
2024-03-16 11:51:18 +0000

    Improving list

--- components/shared/list-item-card.tsx
+++ components/shared/list-item-card.tsx
@@ -1,27 +1,31 @@
 import Image from 'next/image';
 import { MapPinIcon } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
 
 interface ListItemCardProps {
   imageUrl: string;
   numberInList: number;
   location: string;
   name: string;
+  className?: string;
 }
 
 export function ListItemCard({
   imageUrl,
   location,
   name,
-  numberInList
+  numberInList,
+  className
 }: ListItemCardProps) {
   return (
-    <div className="card bg-white w-80 shadow-xl p-px">
+    <div className={clsx(className, 'card bg-white w-80 shadow-xl p-px')}>
       <figure className="relative w-full h-80">
         <Image
           src={imageUrl}
           alt={`#${numberInList} ${name}`}
           fill
           className="object-fill"
+          sizes="100%"
         />
       </figure>
       <div className="card-body text-neutral-900">
