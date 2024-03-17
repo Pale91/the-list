b3d0f6b93f1285bbc595bace261a49e78a6abc08
Pale91 <palmerale91@gmail.com>
2024-03-17 08:19:12 +0000

    Add what is coming list

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Banner from '@/components/landing/banner';
 import { LastCompletedItems } from '@/components/landing/last-completed-items';
+import { WhatIsComing } from '@/components/landing/what-is-coming';
 
 export default async function Home() {
   return (
     <>
       <Banner />
       <div className='p-3'>
+        <WhatIsComing />
+        <div className='mt-5'></div>
         <LastCompletedItems />
       </div>
     </>
