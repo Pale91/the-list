506fe923d0b5968f0ec0ea116bbe4c2b67ecf7ca
Pale91 <palmerale91@gmail.com>
2024-03-22 22:12:42 +0000

    Config for dev mode

--- next.config.js
+++ next.config.js
@@ -1,24 +1,39 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
-        pathname: '/v0/b/the-list-dc90e.appspot.com/**'
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com'
-      }
-    ]
-  },
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['super-duper-umbrella-r7vqwpp5q9whp4r5-3000.app.github.dev', 'localhost:3000'],
-    }
-  }
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+const nextConfig = (phase) => {
+  const devAllowedOrigins = [
+    'super-duper-umbrella-r7vqwpp5q9whp4r5-3000.app.github.dev',
+    'localhost:3000'
+  ];
+
+  return {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'firebasestorage.googleapis.com',
+          port: '',
+          pathname: '/v0/b/the-list-dc90e.appspot.com/**'
+        },
+        {
+          protocol: 'https',
+          hostname: 'lh3.googleusercontent.com'
+        }
+      ]
+    },
+    experimental:
+      phase === PHASE_DEVELOPMENT_SERVER
+        ? {
+            serverActions: {
+              allowedOrigins: [
+                'super-duper-umbrella-r7vqwpp5q9whp4r5-3000.app.github.dev',
+                'localhost:3000'
+              ]
+            }
+          }
+        : undefined
+  };
 };
 
 module.exports = nextConfig;
