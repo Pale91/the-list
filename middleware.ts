3b766f89b6122da57a02d5d8599cffbd4b44f65d
Pale91 <palmerale91@gmail.com>
2024-03-24 11:34:21 +0000

    Securing routes by role

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,22 @@
-export { default } from 'next-auth/middleware';
+import { withAuth } from 'next-auth/middleware';
+import { isUserAdmin } from './infrastructure/roles';
+
+const adminRoutes = ['/desires/create'];
+
+export default withAuth({
+  callbacks: {
+    authorized({ req, token }) {
+      const isAdmin = isUserAdmin(token?.role as string);
+      const url = new URL(req.url);
+
+      if (adminRoutes.includes(url.pathname) && !isAdmin) {
+        return false;
+      }
+
+      return token !== null; // If there is a token, the user is authenticated
+    }
+  }
+});
 
 export const config = {
   matcher: ['/desires/create'],
