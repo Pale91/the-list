55ddc096cca8854aac79ce448dcf4e18aab5a9c4
Pale91 <palmerale91@gmail.com>
2024-03-27 08:32:02 +0000

    Check file type

--- services/activity-service.ts
+++ services/activity-service.ts
@@ -5,9 +5,38 @@ import ActivitySchema, {
   Activity,
   ActivityState
 } from '@/repositories/activity/activity-types';
-import { ZodError, ZodFormattedError } from 'zod';
+import { ZodError, ZodFormattedError, z } from 'zod';
 
-const CreateActivitySchema = ActivitySchema.omit({ id: true });
+const supportedFileTypesByBuffer = [];
+
+const CreateActivitySchema = ActivitySchema.omit({ id: true }).extend({
+  picture: z
+    .instanceof(File)
+    .optional()
+    .refine(
+      async (file: File | undefined) => {
+        if (file === undefined) {
+          return true;
+        }
+
+        // Read the magic number of the file
+        const blobSlice = file.slice(0, 4);
+        const buffer = await blobSlice.arrayBuffer();
+
+        const magicNumber = new Uint8Array(buffer);
+        var header = '';
+        for (var i = 0; i < magicNumber.length; i++) {
+          header += magicNumber[i].toString(16);
+        }
+        console.log('FILE TYPE', header);
+
+        // Add your logic to check the magic number and return true or false accordingly
+      },
+      {
+        message: 'The submitted picture is not a supported file type.'
+      }
+    )
+});
 
 export async function createActivity(
   currentState: ZodFormattedError<Activity> | undefined,
@@ -15,7 +44,7 @@ export async function createActivity(
 ) {
   console.log('planned date', formData.get('plannedDate'));
 
-  const result = CreateActivitySchema.safeParse({
+  const result = await CreateActivitySchema.safeParseAsync({
     name: formData.get('name'),
     description: formData.get('description'),
     location: formData.get('location'),
@@ -24,7 +53,8 @@ export async function createActivity(
     plannedDate: formData.get('plannedDate') || null,
     completionDate: null,
     creationDate: new Date(),
-    referUserId: null
+    referUserId: null,
+    picture: formData.get('file')
   });
 
   if (!result.success) {
@@ -32,6 +62,8 @@ export async function createActivity(
     return result.error.format();
   }
 
-  await addActivity(result.data);
+  const { picture, ...itemData } = result.data;
+
+  await addActivity(itemData);
   console.log('Item added');
 }
